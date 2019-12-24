ea40318e477256eb2585730e7ff0b1ce39adede9
DakDak <yandatson@gmail.com>
2019-12-23 22:42:06 -0500

    added new small projects

---
+++ authorizationserver/src/main/java/resourceserver/src/main/java/com/datson/resourceserver/ResourceserverApplication.java
@@ -0,0 +1,13 @@
+package com.datson.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceserverApplication.class, args);
+    }
+
+}
